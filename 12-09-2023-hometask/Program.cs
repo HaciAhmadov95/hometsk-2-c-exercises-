2c6934a0ba4b298604f9836deba272b26e7bff18
haci1995 <hajiaghaaa@code.edu.az>
2023-09-16 05:15:13 +0400

    first commit

---
+++ 12-09-2023-hometask/Program.cs
@@ -0,0 +1,273 @@
+﻿
+//task1
+
+//int n = 7;
+
+//if (n % 2 == 0)
+//{
+
+//    Console.WriteLine("cutdur");
+
+//}
+
+//else
+//{
+
+//    Console.WriteLine("tekdir");
+
+//}
+
+
+
+
+
+
+
+
+
+//task2
+
+
+
+
+//int m  = 13;
+
+//for (int i = 1; i < m; i++)
+////{
+
+//    Console.WriteLine(i);
+
+//}
+
+
+//task3
+
+
+//int n = 8;
+//int sum = 0;
+//for (int i = 0; i < n; i++)
+//{
+
+//sum+= i;
+
+
+//}
+//Console.WriteLine(sum);
+
+
+
+
+//task4
+
+//int m = 9;
+//int count = 0;
+//for (int i = 0; i < 9; i++)
+//{
+
+//if (i%2 == 1)
+//    {
+
+//        count++;
+
+//    }
+
+//}
+//Console.WriteLine(count);
+
+
+//task5
+
+
+
+
+//int n = 2;
+//int m = 6;
+//int sum = 0;
+
+//for (int i = n; i <= m; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+
+//        sum += i;
+
+//    }
+
+//}
+//Console.WriteLine(sum);
+
+
+//task6
+
+
+//int n = 2;
+//int m = 6;
+//int sum = 0;
+
+//for (int i = n; i <= m; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+
+//        sum += i;
+
+//    }
+
+//}
+//Console.WriteLine(sum);
+
+//int fakt = 1;
+
+//for (int a = sum; a > 0; a--)
+//{
+
+//    fakt *= a;
+
+//}
+
+//Console.WriteLine(fakt);
+
+//task7
+
+
+//int[] nums = { 1, 4, 6, 7, 8 };
+
+//int multiply=1;
+
+//for (int i = 0;i<nums.Length;i++)
+//{
+//if (nums[i] % 2 == 0)
+//    {
+
+//        multiply*= nums[i];
+
+//    }
+
+
+//}
+//Console.WriteLine(multiply);
+
+
+//task8
+
+//int t = 23;
+
+//if (t%3==0 && t%7==0)
+//{
+
+//    Console.WriteLine("bolunur");
+
+//}
+//else
+//{
+
+//    Console.WriteLine("bolunmur");
+
+//}
+//task9
+
+
+
+
+//int n = 2;
+//int m = 5;
+//int sum = 0;
+
+//if (n%2==0 && m%2==0)
+//{
+
+//    sum = m + n;
+//    Console.WriteLine(sum);
+//}
+//else
+//{
+
+//    Console.WriteLine("ededler cut deyil");
+//}
+
+//Task 10
+
+//int n = 4;
+//int m = 7;
+//int sum = 0;
+
+//for (int i = n;i <= m;i++)
+//{
+
+//    if (i%2 == 1)
+//    {
+
+//        sum += i;
+
+//    }
+
+//}
+//Console.WriteLine(sum);
+
+
+
+//task11
+
+
+
+
+
+
+//int[] nums = { 5, 6, 9 };
+
+//int sum = 0;
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+
+//    if (nums[i] % 2 == 1) 
+//    {
+
+
+//        sum += nums[i];
+
+//    }
+
+
+////}
+//Console.WriteLine(sum);
+
+
+
+//task12
+
+//int[] Reqem = { 3, 6, 7, 8, 9 };
+
+//int count = 0;  
+
+//for (int i = 0; i < Reqem.Length; i++)
+//{
+
+//    if (Reqem[i]%2 == 0)
+//    {
+//        count++;
+//    }
+
+//}
+//Console.WriteLine(count);
+
+
+//task13
+
+//int n = 8;
+//int a = 1;
+//for (int i = 1; i < n; i++)
+//{
+//    if (n % i == 0)
+//    {
+//        a++;
+//    }
+//}
+//if (a == 2)
+//{
+//    Console.WriteLine("eded tekdir",n);
+//}
+//else
+//{
+//    Console.WriteLine("eded cutdur");
+//}
